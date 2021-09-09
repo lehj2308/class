ed4726ceb2fa34cf5e1ffd91bf7c87c159a7ea98
lehj2308 <dlvgusvwns@naver.com>
2021-09-09 20:23:04 +0900

    day13 리스너 수업

---
+++ day13/src/day13/Student.java
@@ -0,0 +1,22 @@
+package day13;
+
+public class Student {
+	private String name;
+	private int score;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getScore() {
+		return score;
+	}
+	public void setScore(int score) {
+		this.score = score;
+	}
+	public Student(String name, int score) {
+		this.name=name;
+		this.score=score;
+	}
+}
