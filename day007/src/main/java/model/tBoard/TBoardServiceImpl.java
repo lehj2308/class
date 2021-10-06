b56d1e5a87f91fe0f725d2e6205b325cd6fe028d
lehj2308 <dlvgusvwns@naver.com>
2021-10-06 20:36:02 +0900

    Spring 수업3

--- day007/src/main/java/model/tBoard/TBoardServiceImpl.java
+++ day007/src/main/java/model/tBoard/TBoardServiceImpl.java
@@ -12,39 +12,32 @@ public class TBoardServiceImpl implements TBoardService{
 
 	@Autowired
 	private TBoardDAO tBoardDAO;
-	private LogPlusAdvice logPlusAdvice;
 	
 	public TBoardServiceImpl() {
-		this.logPlusAdvice=new LogPlusAdvice();
 	}
 	
 	@Override
 	public void insertTBoard(TBoardVO vo) {
-		logPlusAdvice.printPlusLog();
 		tBoardDAO.insertTBoard(vo);
 	}
 
 	@Override
 	public void updateTBoard(TBoardVO vo) {
-		logPlusAdvice.printPlusLog();
 		tBoardDAO.updateTBoard(vo);
 	}
 
 	@Override
 	public void deleteTBoard(TBoardVO vo) {
-		logPlusAdvice.printPlusLog();
 		tBoardDAO.deleteTBoard(vo);
 	}
 
 	@Override
 	public List<TBoardVO> getTBoardList(TBoardVO vo) {
-		logPlusAdvice.printPlusLog();
 		return tBoardDAO.getTBoardList(vo);
 	}
 
 	@Override
 	public TBoardVO getTBoard(TBoardVO vo) {
-		logPlusAdvice.printPlusLog();
 		return tBoardDAO.getTBoard(vo);
 	}
 
